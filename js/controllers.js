0feb6d04022136aeee0eb1194717091b009103df
rdanieli <rafaelldanieli@gmail.com>
2015-05-29 23:18:42 -0300

    ajusts

--- js/controllers.js
+++ js/controllers.js
@@ -38,13 +38,38 @@ angular.module('starter.controllers', ['starter.services', 'starter.factories'])
 
 })
 
-.controller('MapController', function($scope, $window, MapFactory) {
+.controller('MapController', function($scope, $window, $ionicModal, MapFactory) {
 
     $scope.startMap = function() {
         var map = MapFactory.init();
 
         google.maps.event.addDomListener($window, 'load', map);
     }
+
+    $ionicModal.fromTemplateUrl('my-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+    $scope.openModal = function() {
+        $scope.modal.show();
+    };
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function() {
+        // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+        // Execute action
+    });
 })
 
 .controller('PlaylistsCtrl', function($scope) {
